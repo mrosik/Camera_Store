b6b5c8219f06bb9c3997b23680d2a99c7ec1431f
WebAppFrameworks <wafuser@wafuser-VirtualBox.(none)>
2012-11-20 21:17:23 +0000

    cart commit

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,45 @@
+class CartController < ApplicationController
+def add
+id = params[:id]
+cart = session[:cart] ||= {}
+cart[id] = (cart[id] || 0) + 1
+redirect_to :action => :index
+end
+
+def index
+@cart = session[:cart] || {}
+end
+
+def change
+cart = session[:cart]
+id = params[:id];
+quantity = params[:quantity].to_i
+if cart and cart[id]
+unless quantity <= 0
+cart[id] = quantity
+else
+cart.delete id
+end
+
+end
+redirect_to :action => :index
+end
+
+def remove
+id = params[:id]
+cart = session[:cart]
+cart.delete id
+redirect_to :action => :index
+end
+
+def clearCart
+session[:cart] = nil
+redirect_to :action => :index
+end
+
+def checkout
+flash[:notice] = "CHECKOUT IS NOT IMPLEMENTED YET!!!"
+redirect_to :action => :index
+end
+end
+
